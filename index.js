6c43732c2c8abb8b4322e368532aad6e78ab186b
nam nguyen <n675n055@ku.edu>
2023-08-21 19:01:02 -0500

    select alls work

--- index.js
+++ index.js
@@ -6,23 +6,69 @@ const db = mysql.createConnection(
 		host: 'localhost',
 		user: 'root',
 		password: 'password',
-		database: 'books_db'
+		database: 'business_db'
 	}
 );
 
 const questions = ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department"]
 
 function init() {
-    inquirer
-      .prompt([//prompts the user with questions
-        {
-          type: 'list',
-          name: 'shape',
-          message: "Please choose a shape for the logo: ",
-          choices: ["Circle", "Triangle", "Square"]
-        }
-          ]).then((data) => {
-              generateSvg(data.text, data.textColor, data.shape, data.shapeColor);//calls generateSVG with
-          });}
-  //calling init function
-  init();
+	inquirer
+		.prompt([//prompts the user with questions
+			{
+				type: 'list',
+				name: 'option',
+				message: "What would you like to do?",
+				choices: [questions[0], questions[1], questions[2], questions[3], questions[4], questions[5], questions[6]]
+			}
+				]).then((data) => {
+					switch(data.option) {
+						case questions[0]:
+							db.query('SELECT * FROM employee', function (err, results) {console.table(results);});
+							break;
+						case questions[1]:
+							addEmployee();
+							break;
+						case questions[2]:
+
+							break;
+						case questions[3]:
+							db.query('SELECT * FROM role', function (err, results) {console.table(results)});
+							break;
+						case questions[4]:
+
+							break;
+						case questions[5]:
+							db.query('SELECT * FROM department', function (err, results) {console.table(results)});
+							break;
+						case questions[6]:
+
+							break;
+					}
+				});}
+
+function addEmployee() {
+	let roles = db.execute('SELECT title, id FROM role');
+	console.log(roles);
+	// inquirer
+	// 	.prompt([
+	// 		{
+	// 			type: 'input',
+	// 			name: 'firstName',
+	// 			message: "What is the employee's first name?"
+	// 		},
+	// 		{
+	// 			type: 'input',
+	// 			name: 'lastName',
+	// 			message: "What is the employee's last name?"
+	// 		},
+	// 		{
+	// 			type: 'list',
+	// 			name: ''
+	// 		}
+	// 			]).then((data) => {
+
+	// 			});
+}
+
+init();
